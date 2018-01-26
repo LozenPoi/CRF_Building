01c79f3553e11060e69dd66af36882046a3f8896
zl5sv <zl5sv.cs.virginia.edu>
2018-01-26 18:43:41 -0500

    cv_edit finished

--- baseline_active_learning_edit.py
+++ baseline_active_learning_edit.py
@@ -8,7 +8,6 @@ from sklearn.model_selection import RandomizedSearchCV
 import matplotlib.pyplot as plt
 import random
 from sklearn.model_selection import RepeatedKFold
-from joblib import Parallel, delayed
 import multiprocessing
 
 import utils
@@ -87,39 +86,36 @@ def cv_edit_active_learn(args):
     test_set = [dataset[i] for i in test_idx]
     train_string = [strings[i] for i in train_idx]
     #test_string = [strings[i] for i in test_idx]
-    X_train = [sent2features(s) for s in train_set]
-    y_train = [sent2labels(s) for s in train_set]
-    X_test = [sent2features(s) for s in test_set]
-    y_test = [sent2labels(s) for s in test_set]
 
     # Define an initial actual training set from the training pool.
-    X_train_current = X_train[:2]
-    y_train_current = y_train[:2]
-    X_train_new = X_train[2:]
-    y_train_new = y_train[2:]
+    train_set_current = train_set[:2]
+    train_set_new = train_set[2:]
     train_string_current = train_string[:2]
     train_string_new = train_string[2:]
+
+    # Obtain testing features and labels.
+    X_test = [sent2features(s) for s in test_set]
+    y_test = [sent2labels(s) for s in test_set]
 
     for num_training in range(max_samples_batch):
 
         # Calculate average distance from new sample candidates to current training set.
         distance = avr_edit_distance(train_string_current, train_string_new)
         sort_idx = np.argsort(-distance, kind='mergesort').tolist()
-        # update strings
+        # update training strings
         string_to_remove = [train_string_new[i] for i in sort_idx[:batch_size]]
         for i in string_to_remove:
             train_string_current.append(i)
             train_string_new.remove(i)
-        # update training features
-        feature_to_remove = [X_train_new[i] for i in sort_idx[:batch_size]]
-        for i in feature_to_remove:
-            X_train_current.append(i)
-            X_train_new.remove(i)
-        # update training labels
-        label_to_remove = [y_train_new[i] for i in sort_idx[:batch_size]]
-        for i in label_to_remove:
-            y_train_current.append(i)
-            y_train_new.remove(i)
+        # update training set
+        sample_to_remove = [train_set_new[i] for i in sort_idx[:batch_size]]
+        for i in sample_to_remove:
+            train_set_current.append(i)
+            train_set_new.remove(i)
+
+        # Obtain current training features.
+        X_train_current = [sent2features(s) for s in train_set_current]
+        y_train_current = [sent2labels(s) for s in train_set_current]
 
         # # define fixed parameters and parameters to search
         # crf = sklearn_crfsuite.CRF(
@@ -176,11 +172,12 @@ if __name__ == '__main__':
     kf = RepeatedKFold(n_splits=num_fold, n_repeats=1, random_state=666)
 
     # Define a loop for plotting figures.
-    max_samples_batch = 5
+    max_samples_batch = 100
     batch_size = 1
 
-    pool = multiprocessing.Pool(os.cpu_count() - 1)
+    pool = multiprocessing.Pool(os.cpu_count()-1)
     args = []
+    # print(os.cpu_count()) # It counts for logical processors instead of physical cores.
     for train_idx, test_idx in kf.split(dataset):
         tmp_args = {
             'train_idx': train_idx,
@@ -192,97 +189,17 @@ if __name__ == '__main__':
         }
         args.append(tmp_args)
     results = pool.map(cv_edit_active_learn, args)
-    print(len(results))
-    print(results[0])
-
-    # cv_round = 0
-    # for train_idx, test_idx in kf.split(dataset):
-
-        # # Define training set and testing set.
-        # train_set = [dataset[i] for i in train_idx]
-        # test_set = [dataset[i] for i in test_idx]
-        # train_string = [strings[i] for i in train_idx]
-        # #test_string = [strings[i] for i in test_idx]
-        # X_train = [sent2features(s) for s in train_set]
-        # y_train = [sent2labels(s) for s in train_set]
-        # X_test = [sent2features(s) for s in test_set]
-        # y_test = [sent2labels(s) for s in test_set]
-        #
-        # # Define an initial actual training set from the training pool.
-        # X_train_current = X_train[:2]
-        # y_train_current = y_train[:2]
-        # X_train_new = X_train[2:]
-        # y_train_new = y_train[2:]
-        # train_string_current = train_string[:2]
-        # train_string_new = train_string[2:]
-        #
-        # for num_training in range(max_samples_batch):
-        #
-        #     # Calculate average distance from new sample candidates to current training set.
-        #     distance = avr_edit_distance(train_string_current, train_string_new)
-        #     sort_idx = np.argsort(-distance, kind='mergesort').tolist()
-        #     # update strings
-        #     string_to_remove = [train_string_new[i] for i in sort_idx[:batch_size]]
-        #     for i in string_to_remove:
-        #         train_string_current.append(i)
-        #         train_string_new.remove(i)
-        #     # update training features
-        #     feature_to_remove = [X_train_new[i] for i in sort_idx[:batch_size]]
-        #     for i in feature_to_remove:
-        #         X_train_current.append(i)
-        #         X_train_new.remove(i)
-        #     # update training labels
-        #     label_to_remove = [y_train_new[i] for i in sort_idx[:batch_size]]
-        #     for i in label_to_remove:
-        #         y_train_current.append(i)
-        #         y_train_new.remove(i)
-        #
-        #     # # define fixed parameters and parameters to search
-        #     # crf = sklearn_crfsuite.CRF(
-        #     #     algorithm='lbfgs',
-        #     #     max_iterations=100,
-        #     #     all_possible_transitions=True
-        #     # )
-        #     # params_space = {
-        #     #     'c1': scipy.stats.expon(scale=0.5),
-        #     #     'c2': scipy.stats.expon(scale=0.05),
-        #     # }
-        #     #
-        #     # # search
-        #     # rs = RandomizedSearchCV(crf, params_space,
-        #     #                         cv=2,
-        #     #                         verbose=1,
-        #     #                         n_jobs=-1,
-        #     #                         n_iter=5)
-        #     # rs.fit(X_train_current, y_train_current)
-        #     #
-        #     # print('best params:', rs.best_params_)
-        #     # print('best CV score:', rs.best_score_)
-        #     # crf = rs.best_estimator_
-        #
-        #     # Train the CRF.
-        #     crf = sklearn_crfsuite.CRF(
-        #         algorithm='lbfgs',
-        #         c1=0.1,
-        #         c2=0.1,
-        #         max_iterations=100,
-        #         all_possible_transitions=True
-        #     )
-        #     crf.fit(X_train_current, y_train_current)
-        #
-        #     # Use the estimator.
-        #     y_pred = crf.predict(X_test)
-        #     phrase_count, phrase_correct, out_count, out_correct = utils.phrase_acc(y_test, y_pred)
-        #     print(phrase_count, phrase_correct, out_count, out_correct)
-        #     phrase_acc[num_training,cv_round] = phrase_correct/phrase_count
-        #     out_acc[num_training,cv_round] = out_correct/out_count
-
-        # cv_round += 1
+    # print(len(results))
+    # print(len(results[0]))
+    phrase_acc = [results[i][0] for i in range(num_fold)]
+    out_acc = [results[i][1] for i in range(num_fold)]
+    # print(len(phrase_acc))
+    # print(len(phrase_acc[0]))
 
     phrase_acc_av = np.sum(phrase_acc, axis=0)/num_fold
     out_acc_av = np.sum(out_acc, axis=0)/num_fold
-    plt.plot(np.arange(3,max_samples_batch*batch_size+3,batch_size),phrase_acc,'r',
-             np.arange(3,max_samples_batch*batch_size+3,batch_size),out_acc,'b')
+    plt.plot(np.arange(3,max_samples_batch*batch_size+3,batch_size),phrase_acc_av,'r',
+             np.arange(3,max_samples_batch*batch_size+3,batch_size),out_acc_av,'b')
     plt.xlabel('number of training samples')
     plt.ylabel('testing accuracy')
     plt.legend(['phrase accuracy', 'out-of-phrase accuracy'])
--- baseline_active_learning_uniform.py
+++ baseline_active_learning_uniform.py
@@ -1,5 +1,16 @@
-from sklearn.model_selection import KFold
+import os
+import pickle
+import numpy as np
+import editdistance
+import sklearn_crfsuite
+import scipy.stats
+from sklearn.model_selection import RandomizedSearchCV
+import matplotlib.pyplot as plt
+import random
 from sklearn.model_selection import RepeatedKFold
+import multiprocessing
+
+import utils
 
 X = [["a", "b"], ["c", "d"], ["e", "f", "g", "h"]]
 kf = RepeatedKFold(n_splits=3, n_repeats=1, random_state=666)
